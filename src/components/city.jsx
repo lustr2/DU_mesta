7bab62151b70ac84d76b59efbf95aa15795b50c8
Lucie Struplova <lstruplova@gmail.com>
2024-02-16 22:35:32 +0100

    Ukol-mesta

---
+++ src/components/city.jsx
@@ -0,0 +1,14 @@
+const City = ({name, population, area, district, photo}) => {
+    const vetsiMesto = (parseInt(area) > 100 ? "item__vetsi" : "item__normalni");
+      return (
+        <div className="mesta">
+            <h3>{name}</h3>
+            <div><img src={photo} alt=''></img></div>
+            <div className="populace"> Pocet obyvatel: {population} </div>
+            <div className={vetsiMesto}> Vetsi mesto:  </div>
+            <div> Okres: {district}</div>
+            
+        </div>
+    )
+}
+export default City;
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,19 @@
-import './App.css';
-import City from './components/city';
+import { createRoot } from 'react-dom/client';
+import City from './components/city/city';
 import { cities } from './cz-cities';
 import './global.css';
 
-function App() {
-  return (
-    <div className="App">
-        <main>
+const App = () => (
+        <>
+      <main>
         <article>
           <div className="container">{cities.map(c => <div><City key={c.name} name={c.name} population={c.population} area={c.area} district={c.district} photo={c.photo}></City></div>)}</div>
         </article>
       </main>
-
-    </div>
+    </>
   );
-}
-
-export default App;
+  
+  createRoot(
+    document.querySelector('#app'),
+  ).render(<App />);
+  
